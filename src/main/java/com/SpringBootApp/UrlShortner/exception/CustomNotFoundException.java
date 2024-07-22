98940539726430c8a84df4c783dbcfc0fc590c6e
josephC45 <joey.curtog@gmail.com>
2024-07-21 21:17:38 -0400

    remove CustomNotFoundException class file

--- src/main/java/com/SpringBootApp/UrlShortner/exception/CustomNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.SpringBootApp.UrlShortner.exception;
-
-public class CustomNotFoundException extends RuntimeException{
-    public CustomNotFoundException(String message){
-        super(message);
-    }
-}
