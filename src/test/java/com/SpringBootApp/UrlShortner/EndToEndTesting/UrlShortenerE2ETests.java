633294d19c1c95f2b4ddb9cc70d2df7d92a9c8d8
josephC45 <joey.curtog@gmail.com>
2025-01-10 19:07:05 -0500

    format file

--- src/test/java/com/SpringBootApp/UrlShortner/EndToEndTesting/UrlShortenerE2ETests.java
+++ src/test/java/com/SpringBootApp/UrlShortner/EndToEndTesting/UrlShortenerE2ETests.java
@@ -16,19 +16,19 @@ public class UrlShortenerE2ETests {
 
         String longUrl = "http://localhost:8080/abcdefg1234567";
         Response response = RestAssured
-            .given()
-            .contentType("application/json")
-            .body(longUrl)
-            .post("/api/v1/urls");
+                .given()
+                .contentType("application/json")
+                .body(longUrl)
+                .post("/api/v1/urls");
         assertEquals(201, response.getStatusCode());
         String shortUrl = response.jsonPath().getString("shortUrl");
         assertNotNull(shortUrl);
 
         Response retrieveLongUrlFromShortUrl = RestAssured
-            .given()
-            .queryParam("shortUrl", shortUrl)
-            .get("/api/v1/urls");
-        assertEquals(200,retrieveLongUrlFromShortUrl.getStatusCode());
+                .given()
+                .queryParam("shortUrl", shortUrl)
+                .get("/api/v1/urls");
+        assertEquals(200, retrieveLongUrlFromShortUrl.getStatusCode());
         String longUrlAssociatedWithNewlyCreatedShortUrl = retrieveLongUrlFromShortUrl.jsonPath().getString("url");
         assertEquals(longUrlAssociatedWithNewlyCreatedShortUrl, longUrl);
     }
@@ -39,25 +39,25 @@ public class UrlShortenerE2ETests {
 
         String longUrl = "http://localhost:8080/abcdefg12345678";
         Response creationResponse = RestAssured
-            .given()
-            .contentType("application/json")
-            .body(longUrl)
-            .post("/api/v1/urls");
+                .given()
+                .contentType("application/json")
+                .body(longUrl)
+                .post("/api/v1/urls");
 
         assertEquals(201, creationResponse.getStatusCode());
         String shortUrl = creationResponse.jsonPath().getString("shortUrl");
         assertNotNull(shortUrl);
 
         Response deletionResponse = RestAssured
-            .given()
-            .queryParam("shortUrl", shortUrl)
-            .delete("/api/v1/urls");
+                .given()
+                .queryParam("shortUrl", shortUrl)
+                .delete("/api/v1/urls");
         assertEquals(204, deletionResponse.getStatusCode());
 
         Response getUrlThatDoesNotExistResponse = RestAssured
-            .given()
-            .queryParam("shortUrl", shortUrl)
-            .get("/api/v1/urls");
+                .given()
+                .queryParam("shortUrl", shortUrl)
+                .get("/api/v1/urls");
         assertEquals(404, getUrlThatDoesNotExistResponse.getStatusCode());
     }
 }
