928743794a1c62ddd7130294728b748fe181c51e
josephC45 <joey.curtog@gmail.com>
2025-03-04 15:43:23 -0500

    add prometheus request counting and latency tracking

--- src/main/java/com/SpringBootApp/UrlShortner/rest/MyRestController.java
+++ src/main/java/com/SpringBootApp/UrlShortner/rest/MyRestController.java
@@ -1,5 +1,7 @@
 package com.SpringBootApp.UrlShortner.rest;
 
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +18,9 @@ import com.SpringBootApp.UrlShortner.dto.UrlDto;
 import com.SpringBootApp.UrlShortner.entity.Url;
 import com.SpringBootApp.UrlShortner.service.UrlService;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -23,9 +28,13 @@ import reactor.core.publisher.Mono;
 public class MyRestController {
 
     private UrlService urlService;
+    private final Counter shortenUrlCounter;
+    private final Timer shortenUrlLatency;
 
-    public MyRestController(UrlService urlService) {
+    public MyRestController(UrlService urlService, MeterRegistry meterRegistry) {
         this.urlService = urlService;
+        this.shortenUrlCounter = meterRegistry.counter("url_shorten_requests_total");
+        this.shortenUrlLatency = meterRegistry.timer("url_shorten_request_latency");
     }
 
     @GetMapping
@@ -34,14 +43,21 @@ public class MyRestController {
                 .map(urlDto -> ResponseEntity.ok().body(urlDto))
                 .defaultIfEmpty(ResponseEntity.notFound().build());
     }
-
     @PostMapping
     public Mono<ResponseEntity<Url>> createUrl(@RequestBody String longUrl, UriComponentsBuilder ucb) {
-        return urlService.createUrl(longUrl)
-                .map(createdUrl -> {
-                    UriComponents uriComponents = ucb.path("/api/v1/urls/{id}").buildAndExpand(createdUrl.getId());
-                    return ResponseEntity.created(uriComponents.toUri()).body(createdUrl);
-                });
+        Mono<ResponseEntity<Url>> newUrl = null;
+        long start = System.nanoTime();
+        try {
+            newUrl = urlService.createUrl(longUrl)
+                    .map(createdUrl -> {
+                        shortenUrlCounter.increment();
+                        UriComponents uriComponents = ucb.path("/api/v1/urls/{id}").buildAndExpand(createdUrl.getId());
+                        return ResponseEntity.created(uriComponents.toUri()).body(createdUrl);
+                    });
+        } finally {
+            shortenUrlLatency.record(System.nanoTime() - start, TimeUnit.NANOSECONDS);
+        }
+        return newUrl;
     }
 
     @DeleteMapping
